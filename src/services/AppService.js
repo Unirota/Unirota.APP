66c68a95e33793d5710f7bc796db63cacf89439c
Thiago Arcoverde <tluiz2701@gmail.com>
2024-10-30 19:27:16 -0300

    Features/#22618 - Remoção de código desnecessário

--- src/services/AppService.js
+++ src/services/AppService.js
@@ -4,12 +4,10 @@ import AlertAsync from "react-native-alert-async"
 import { BackHandler } from "react-native"
 export default new class AppService {
     async Login(email, password) {
-        console.log(1)
         let response = await ApiUnirota.post("/Auth", {
             email: email,
             senha: password
         }).then(async response => {
-            console.log(2)
             await AsyncStorage.setItem('email', email)
             await AsyncStorage.setItem('password', password)
             ApiUnirota.defaults.headers.common['Authorization'] = `Bearer ${response.data.accessToken}`
