f7e554127873e2307772f56bad8ac2f8d62f5d9b
Luana Beatriz Alvares <luanabalvares@outlook.com>
2024-11-10 09:41:38 -0300

    Features/#22949 - Modificando o InitialRoute

--- App.js
+++ App.js
@@ -35,7 +35,7 @@ export default class App extends Component {
       )
     }
 
-    let initialRoute = isAuthorized ? 'HomePage' : ""
+    let initialRoute = isAuthorized ? 'HomePage' : "LoginPage"
 
     return (
       <FontProvider>
