43613e9fe7ce35977c075463126d0e10eda23ebc
José Ricardo <josericardomgomes@gmail.com>
2024-11-20 19:44:56 -0300

    bugfix/#23447 - Navegação para tela de 'Quero ser motorista'

---
+++ src/styles/Molecules/HeaderRegisterDriverStyles.js
@@ -0,0 +1,49 @@
+import { StyleSheet } from "react-native";
+export default StyleSheet.create({
+    titleLogin: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        alignSelf: 'center'
+    },
+    icon: {
+        alignSelf: 'center',
+        marginTop: 60
+    },
+    subtitleLogin: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        alignSelf: 'center' 
+    },
+    subtitleRegister: {
+        marginTop: 30,
+        marginLeft: 60,
+        flexDirection: 'row',
+    
+    },
+    commonSubtitleLogin: {
+        fontSize: 20,
+        fontWeight: '600',
+        fontFamily: 'Inter',
+    },
+    commonTextLogin: {
+        margin: 2,
+        fontSize: 40,
+        fontWeight: '800',
+        fontFamily: 'Inter'
+    },
+    commonTextRegisterDriver: {
+        margin: 2,
+        fontSize: 30,
+        fontWeight: '800',
+        fontFamily: 'Inter'
+    },
+    iconUser: {
+        alignSelf: 'center',
+        marginTop: 30
+    },
+    styleNoneImageUser: {
+        display: 'none',
+    }
+})
