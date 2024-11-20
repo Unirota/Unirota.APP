dd2eb118da02bd15cf3714670050dffc7b1c4cc1
José Ricardo <84511837+MiessiGomes@users.noreply.github.com>
2024-11-20 16:56:55 -0300

    #23391 - Implementação da modal da tela de solicitações (#36)
    * Features/#23391 - Implementação de Modal para tela de Convite
    
    * Features/#23391 - Implementação da modal da tela de Solicitações

--- src/ui/Molecules/RequestCard.js
+++ src/ui/Molecules/RequestCard.js
@@ -3,9 +3,17 @@ import { View, TouchableOpacity, Text } from 'react-native'
 import GroupAvatar from '../Atoms/SearchGroupAvatar'
 import GroupInfo from '../Atoms/SearchGroupInfo'
 import RequestInfoButton from '../Atoms/RequestInfoButton'
+import RequestGroupModal from './RequestGroupModal'
 import RequestCardStyles from '../../styles/Molecules/RequestCardStyles'
 
 export default class RequestCard extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      isModalVisible: false,
+  }
+}
+
   formatTime(time) {
     if (!time) return '';
     
@@ -22,8 +30,13 @@ export default class RequestCard extends Component {
     }
   }
 
+  toggleModal = () => {
+    this.setState((prevState) => ({ isModalVisible: !prevState.isModalVisible }));
+  };
+
   render() {
     const { group } = this.props
+    const { isModalVisible } = this.state
     const formattedTime = this.formatTime(group.horaInicio)
 
     return (
@@ -38,11 +51,18 @@ export default class RequestCard extends Component {
         <View style={RequestCardStyles.rightContent}>
           <Text style={RequestCardStyles.timeText}>{formattedTime}</Text>
           <View style={RequestCardStyles.buttonsContainer}>
-            <TouchableOpacity>
-              <RequestInfoButton/>
+            <TouchableOpacity onPress={this.toggleModal}>
+              <RequestInfoButton onPress={this.toggleModal}/>
             </TouchableOpacity>
           </View>
         </View>
+
+        <RequestGroupModal
+          group={group}
+          isVisible={isModalVisible}
+          onClose={this.toggleModal}
+        />
+
       </View>
     )
   }
