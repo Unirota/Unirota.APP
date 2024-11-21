0790b6412353e36bb0dfa33682abf4931dfa0f66
Thiago Arcoverde <tluiz2701@gmail.com>
2024-11-21 00:48:23 -0300

    Criacao inicial da tela de dados do grupo

--- src/ui/Atoms/GroupDataHeader.js
+++ src/ui/Atoms/GroupDataHeader.js
@@ -1,25 +1,45 @@
 import React, { Component } from 'react'
 import { View, Text } from 'react-native'
-import { GroupDataAvatarHeaderStyles } from '../../styles/Atoms/GroupDataAvatarHeaderStyles'
-import { GrupoDataHeaderStyles } from '../../styles/Atoms/GrupoDataHeaderStyles'
-
-
-//uma view geral dentro dela icone, titulo do grupo e quantidade de membro. Para cada um deles vai ter sua view 
-
+import { StyleSheet } from 'react-native'
 export default class GroupDataHeader extends Component {
-    render() {
-      return (
-        <View style={GroupDataAvatarHeaderStyles.container}>
-          <View style={GroupDataAvatarHeaderStyles.icongroupdata}>
-  
-          </View>
-          <View style={GrupoDataHeaderStyles.textgroupname}>
-            <Text style={GrupoDataHeaderStyles.firstTitle}>Grupo Alpha</Text>
-          </View>
-          <View style={GrupoDataHeaderStyles.textgroupmembers}>
-            <Text style={GrupoDataHeaderStyles.secondTitle}>4 membros</Text>
-          </View>
+  render() {
+    return (
+      <View style={styles.container}>
+        <View>
+          <Text style={styles.title}>Dados do grupo</Text>
         </View>
-      )
-    }
+        <View>
+          <Text style={styles.groupName}>Grupo Alpha</Text>
+        </View>
+        <View>
+          <Text style={styles.members}>4 membros</Text>
+        </View>
+      </View>
+    )
   }
+} 
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 32,
+    marginBottom: 40
+  },
+  title: {
+    color: 'white',
+    alignSelf: 'center',
+    fontSize: 16,
+    fontFamily: 'Inter',
+    marginBottom: 40
+  },
+  groupName: {
+    color: 'white',
+    alignSelf: 'center',
+    fontSize: 24,
+    fontFamily: 'Inter'
+  },
+  members: {
+    color: '#C3C3C3',
+    alignSelf: 'center',
+    fontSize: 16,
+  }
+})
--- src/ui/Organisms/GroupData/index.js
+++ src/ui/Organisms/GroupData/index.js
@@ -3,17 +3,15 @@ import { View, ScrollView } from 'react-native'
 import { LinearGradient } from 'expo-linear-gradient'
 import NavigationFooter from '../../Molecules/NavigationFooter'
 import GroupDataHeader from '../../Atoms/GroupDataHeader'
-import { GroupDataAvatarHeaderStyles } from '../../../styles/Atoms/GroupDataAvatarHeaderStyles'
+import GroupDataContainer from '../../Atoms/GroupDataContainer'
 export default class GroupDataPage extends Component {
   render() {
     return (
-      <LinearGradient colors={['#00112B', '#003A90']}>
-       
-          <View>
-            <GroupDataAvatarHeaderStyles/>
-            <GroupDataHeader navigation={this.props.navigation} />
+      <LinearGradient colors={['#00112B', '#003A90']} style={{flex: 1}}>
+          <View style={{flex: 1}}>
+            <GroupDataHeader />
+            <GroupDataContainer/>
           </View>
-        
         <NavigationFooter navigation={this.props.navigation} />
       </LinearGradient>
     )
