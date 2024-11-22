04c7c6d08dbf516a4d8d6b91a2bcd5c0c8547928
José Ricardo <84511837+MiessiGomes@users.noreply.github.com>
2024-11-22 00:07:05 -0300

    bugfix/#23502 - Correção de bugs visuais na tela de FAQ (#48)

--- src/services/FaqService.js
+++ src/services/FaqService.js
@@ -1,6 +1,7 @@
 export const FaqData = [
   {
     id: 1,
+    question: 'O que é o Unirota?',
     answer:
       'O UniRota é um aplicativo de carona universitária, desenvolvido especialmente para estudantes que desejam compartilhar o trajeto até a universidade de forma segura e prática. O foco do UniRota é conectar estudantes que possuem carro com outros estudantes que precisam de uma carona, promovendo a economia compartilhada, reduzindo o custo com transporte e contribuindo para uma mobilidade mais sustentável no ambiente universitário.',
   },
@@ -8,30 +9,18 @@ export const FaqData = [
     id: 2,
     question: 'Como funciona o Unirota?',
     answer:
-      'O UniRota funciona como uma plataforma de intermediação entre motoristas e passageiros universitários. O usuário pode se cadastrar como passageiro ou motorista, sendo que motoristas podem oferecer caronas para os trajetos até a universidade. Através do aplicativo, os passageiros podem visualizar motoristas disponíveis, escolher uma carona que atenda a suas necessidades, e, ao final, ambos os usuários podem avaliar a experiência para manter um ambiente seguro e de confiança.',
+      'O UniRota funciona como uma plataforma de intermediação entre motoristas e passageiros universitários. O usuário pode se cadastrar como passageiro ou motorista, sendo que motoristas podem oferecer caronas para os trajetos até a universidade. Através do aplicativo, os passageiros podem visualizar grupos disponíveis, escolher uma carona que atenda a suas necessidades, e, ao final, ambos os usuários podem avaliar a experiência para manter um ambiente seguro e de confiança.',
   },
   {
     id: 3,
     question: 'Como criar meu grupo de caronas?',
     answer:
-      'Para criar um grupo de caronas no UniRota, o usuário deve acessar a seção de grupos no aplicativo e selecionar a opção para criar um novo grupo. A partir daí, ele pode definir o nome, as preferências e os critérios de aceitação do grupo, como horários de saída, pontos de encontro e o público-alvo específico. Após configurar o grupo, ele estará disponível para que outros usuários interessados possam solicitar a participação, tornando o processo de organização de caronas mais eficiente e segmentado.',
+      'Para criar um grupo de caronas no UniRota, o usuário deve acessar o seu perfil no aplicativo e selecionar a opção "Criar grupo de carona". A partir daí, ele pode definir o nome, quantidade máxima de pessoas, hora de início, destino e uma descrição. Após configurar o grupo, ele estará disponível para que outros usuários interessados possam solicitar a participação, tornando o processo de organização de caronas mais eficiente e segmentado.',
   },
   {
     id: 4,
     question: 'Como me tornar um motorista?',
     answer:
       'Para se tornar um motorista no UniRota, é necessário que o usuário primeiro faça o cadastro como passageiro e, em seguida, habilite a função de motorista no seu perfil. O aplicativo solicitará algumas informações adicionais, como a verificação dos documentos do carro e da habilitação. Após a validação dos documentos pela equipe do UniRota, o usuário estará apto a oferecer caronas para outros estudantes, que poderão visualizar o seu perfil e entrar em contato para combinar os detalhes da viagem.',
-  },
-  {
-    id: 5,
-    question: 'Como realizar uma denúncia?',
-    answer:
-      'Caso ocorra qualquer incidente ou situação que precise ser reportada, o UniRota conta com uma ferramenta de denúncias acessível diretamente pelo aplicativo. Basta acessar o perfil do usuário ou a carona específica, selecionar a opção de denúncia e detalhar o motivo. A equipe de suporte irá avaliar a situação e tomar as medidas necessárias para garantir a segurança e a qualidade do serviço para todos os usuários.',
-  },
-  {
-    id: 6,
-    question: 'Como deletar a minha conta?',
-    answer:
-      'Para deletar sua conta no UniRota, vá até a seção de configurações do aplicativo e selecione a opção "Deletar conta". Após confirmar a ação, o sistema iniciará o processo de exclusão dos dados associados ao perfil. Tenha em mente que a exclusão é definitiva e todos os dados e históricos de carona serão removidos do sistema.',
   },
 ]
